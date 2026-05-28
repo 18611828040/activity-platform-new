ff4d83588ca48cc2467f247487db54e5d0b7588d
whiter8023 <whiter8023@whiter8023deMacBook-Air.local>
2026-05-28 11:42:09 +0800

    chore: track out/ directory for GitHub Actions

---
+++ out/_next/static/HveqNF5HCaxmOszvvUYIp/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Factivity-analysis\u002F[id]","\u002Factivity-management\u002F[id]","\u002Ftask-analysis\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
